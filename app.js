92b397518a86956607426887bebc8a5a952d54e6
jakehdchoi <jakehdchoi@gmail.com>
2018-06-16 03:43:06 +0900

    execution time log added

--- app.js
+++ app.js
@@ -5,18 +5,35 @@ var config = require('./config.js');
 
 var Twitter = new twit(config);
 
-var userList = { screen_name: 'jakehdchoi', count: 3, tweet_mode: 'extended' };
+var userList = [
+    'jakehdchoi',
+    'rogerkver',
+    'VitalikButerin'
+]
 
-Twitter.get('statuses/user_timeline', userList , function(err, data) {
-    console.log(data);
-    for (var i = 0; i < data.length ; i++) {
-    console.log(data[i].full_text);
-  }
-})
+const before = Date.now();
 
-
-
-
+for (var i = 0; i < userList.length; i++) {
+    Twitter.get('statuses/user_timeline', {
+        screen_name: userList[i],
+        count: 20,
+        tweet_mode: 'extended'
+    }, function(err, data) {
+        // console.log(data);
+        if (err) {
+            console.log(err);
+        } else {
+            for (var j = 0; j < data.length; j++) {
+                console.log(data[j].created_at);
+                console.log(data[j].user.screen_name);
+                console.log(data[j].full_text);
+                console.log('https://twitter.com/i/web/status/' + data[j].id_str);
+                console.log((Date.now() - before) / 1000);
+                console.log('');
+            }
+        }
+    })
+}
 
 
 
